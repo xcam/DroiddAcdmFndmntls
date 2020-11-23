00d54100f8da793bc6f43321e8d6430e3c3a7405
Андрей Кузнецов <a.kuznetsov3@atol.ru>
2020-11-24 00:13:00 +0300

    Homework1

---
+++ app/src/main/java/ru/xcam/droiddacdmfndmntls/moviedetails/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package ru.xcam.droiddacdmfndmntls.moviedetails
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.xcam.droiddacdmfndmntls.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
